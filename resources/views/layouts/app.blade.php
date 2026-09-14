7efc157904b6d7e1dd080fc799398513320269ec
hfdzz <yhafidz@gmail.com>
2026-09-14 18:11:39 +0700

    add(footer): copyright and social links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -201,11 +201,13 @@
                 @yield('content')
             </main>
 
-            <footer class="site-footer">
+        </div>
+        <footer class="site-footer">
+            <div class="site-footer-row site-footer-row-primary">
                 <div class="site-footer-primary">
                     <a href="{{ route('laws.index', ['lang' => $currentLanguage]) }}" class="site-footer-brand" aria-label="{{ __('site.nav.go_home') }}">
                         <img src="{{ asset('statics/logo_pssi_tulisan.png') }}" alt="PSSI">
-                        <span>{{ __('site.brand') }}</span>
+                        <!-- <span>{{ __('site.brand') }}</span> -->
                     </a>
                     <p>{{ __('site.footer.description') }}</p>
                     <p class="site-footer-edition">
@@ -226,7 +228,54 @@
                         <a href="{{ route('qas.index', ['lang' => $currentLanguage]) }}">{{ __('site.nav.qas') }}</a>
                     @endif
                 </nav>
-            </footer>
-        </div>
+            </div>
+            <div class="site-footer-row site-footer-row-secondary">
+                <div class="site-footer-copyright">
+                    <p>PSSI - Football Association of Indonesia © 2026. All Rights Reserved</p>
+                </div>
+                <div class="site-footer-social">
+                    <ul>
+                        <li>
+                            <a href="https://www.pssi.org" rel="nofollow noopener noreferrer" target="_blank">
+                                <span class="sr-only">PSSI</span>
+                                <img class="site-footer-social-logo" src="{{ asset('storage/logos/pssi_bnw.png') }}" alt="">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/pssi" rel="nofollow noopener noreferrer" target="_blank">
+                                <span class="sr-only">Facebook</span>
+                                <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+                                    <path d="M4.8 3h14.4A1.8 1.8 0 0 1 21 4.8v14.4a1.8 1.8 0 0 1-1.8 1.8h-3.7v-6.8h2.3l.4-2.8h-2.7V9.6c0-.8.2-1.4 1.4-1.4h1.5V5.7c-.7-.1-1.4-.1-2.1-.1-2.1 0-3.6 1.3-3.6 3.7v2.1h-2.4v2.8h2.4V21H4.8A1.8 1.8 0 0 1 3 19.2V4.8A1.8 1.8 0 0 1 4.8 3Z" fill="currentColor"></path>
+                                </svg>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/pssi" rel="nofollow noopener noreferrer" target="_blank">
+                                <span class="sr-only">Twitter</span>
+                                <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+                                    <path d="M13.9 10.6 21.2 2h-1.7l-6.4 7.4L8.1 2H2.3l7.7 11.2L2.3 22h1.8l6.7-7.8 5.4 7.8h5.8l-8-11.4Zm-2.4 2.8-.8-1.1L4.5 3.3h2.8l5 7.2.8 1.1 6.5 9.2h-2.8l-5.3-7.4Z" fill="currentColor"></path>
+                                </svg>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.youtube.com/channel/UCbOdQk9540V09ff51nFEDKw" rel="nofollow noopener noreferrer" target="_blank">
+                                <span class="sr-only">Youtube</span>
+                                <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+                                    <path d="M21.6 7.2s-.2-1.6-.9-2.3c-.9-.9-1.9-.9-2.3-1C15.2 3.7 12 3.7 12 3.7h0s-3.2 0-6.4.2c-.5.1-1.5.1-2.3 1-.7.7-.9 2.3-.9 2.3S2.2 9.1 2.2 11v1.8c0 1.9.2 3.8.2 3.8s.2 1.6.9 2.3c.9.9 2 .9 2.5 1 1.8.2 6.2.2 6.2.2s3.2 0 6.4-.2c.5-.1 1.5-.1 2.3-1 .7-.7.9-2.3.9-2.3s.2-1.9.2-3.8V11c0-1.9-.2-3.8-.2-3.8ZM10 15.1V8.5l5.9 3.3-5.9 3.3Z" fill="currentColor"></path>
+                                </svg>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/pssi/" rel="nofollow noopener noreferrer" target="_blank">
+                                <span class="sr-only">Instagram</span>
+                                <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+                                    <path d="M7.8 2h8.4A5.8 5.8 0 0 1 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8A5.8 5.8 0 0 1 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2Zm8.4 18.2a4 4 0 0 0 4-4V7.8a4 4 0 0 0-4-4H7.8a4 4 0 0 0-4 4v8.4a4 4 0 0 0 4 4h8.4ZM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10Zm0 8.2A3.2 3.2 0 1 0 12 8.8a3.2 3.2 0 0 0 0 6.4Zm5.2-8.4a1.2 1.2 0 1 1 0-2.4 1.2 1.2 0 0 1 0 2.4Z" fill="currentColor"></path>
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
     </body>
 </html>
